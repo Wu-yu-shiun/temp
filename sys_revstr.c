8164767c9a80483c8249ee8e70e93f9180ad0ffb
Wishhhhh <86937408+Wu-yu-shiun@users.noreply.github.com>
2024-10-01 18:29:41 +0800

    Update sys_revstr.c

--- sys_revstr.c
+++ sys_revstr.c
@@ -29,3 +29,45 @@ SYSCALL_DEFINE2(sys_revstr, char *, str, size_t, len) {
 
     return 0;
 }
+
+
+//////
+#include <linux/syscalls.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+
+SYSCALL_DEFINE2(revstr, int, length, char __user *, str) {
+    // 動態分配記憶體，用於儲存來自用戶空間的字串
+    char *buffer = kmalloc(sizeof(char) * (length + 1), GFP_KERNEL);
+    int i, j;
+    unsigned long len = length;
+
+    // 檢查記憶體是否分配成功
+    if (!buffer)
+        return -ENOMEM;
+
+    // 從用戶空間複製字串到內核空間
+    if (copy_from_user(buffer, str, len + 1)) {
+        kfree(buffer);
+        return -EFAULT;
+    }
+
+    // 打印原始字串到內核ring buffer，dmesg可查看
+    printk(KERN_INFO "The origin string: %s\n", buffer);
+
+    // 反轉字串
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        char temp = buffer[i];
+        buffer[i] = buffer[j];
+        buffer[j] = temp;
+    }
+
+    // 打印反轉後的字串到內核ring buffer
+    printk(KERN_INFO "The reversed string: %s\n", buffer);
+
+    // 釋放分配的記憶體
+    kfree(buffer);
+
+    // 返回 0 表示成功
+    return 0;
+}
