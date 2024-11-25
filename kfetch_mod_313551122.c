1775d5cdb537613e92b126f69fe370f009d854f3
Wishhhhh <86937408+Wu-yu-shiun@users.noreply.github.com>
2024-11-25 16:35:06 +0800

    Update kfetch_mod_313551122.c

--- kfetch_mod_313551122.c
+++ kfetch_mod_313551122.c
@@ -25,74 +25,80 @@
 
 #define KFETCH_FULL_INFO ((1 << 6) - 1)
 
-// 全域變數
 static char *kfetch_buf;
 static int info_mask = KFETCH_FULL_INFO;
 static int major_number;
 static struct class *cls;
-static DEFINE_MUTEX(kfetch_mutex); // 使用 mutex 處理多線程安全
+static DEFINE_MUTEX(kfetch_mutex);
 
-// 設備文件操作 - read
+
+
 static ssize_t kfetch_read(struct file *file, char __user *user_buf, size_t len, loff_t *offset) {
     int buf_len = 0;
     struct sysinfo si;
     struct task_struct *task;
     int proc_count = 0;
+    int line = 0;
+    char linux_logo[7][50] = {
+        "         .-.        ",
+        "        (.. |       ",
+        "        \033[33m<>\033[0m  |       ",
+        "       / --- \\      ",
+        "      ( |   | )     ",
+        "    \033[33m|\\\033[0m\\_)__(_/\033[33m/|\033[0m    ",
+        "   \033[33m<__)\033[0m------\033[33m(__>\033[0m   "
+    };
 
-    if (*offset != 0) // 單次讀取
+    if (*offset != 0)
         return 0;
 
     mutex_lock(&kfetch_mutex);
 
-    // 輸出 logo，\033[33m變黃，\033[0m變白
-    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                         "      .-.     \n"
-                         "     (.. |    \n"
-                         "     \033[33m<>\033[0m  |    \n"
-                         "    / --- \\   \n"
-                         "   ( |   | )  \n"
-                         " \033[33m|\\\033[0m_)__(_/\033[33m/|\033[0m \n"
-                         "\033[33m<__)\033[0m------\033[33m(__>\033[0m\n");
+    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"                    ");
+    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"%s\n", utsname()->nodename);
 
-    // 加入 hostname 和分隔線
-    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                         "%s\n", utsname()->nodename);
-    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                         "--------------------------\n");
+    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
+    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"----------------------------------------\n");
 
-    // 根據 info_mask 選擇性輸出資訊
+    buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     if (info_mask & KFETCH_RELEASE) {
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "Kernel: %s\n", utsname()->release);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"Kernel: %s\n", utsname()->release);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     }
 
     if (info_mask & KFETCH_CPU_MODEL) {
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "CPU: QEMU Virtual CPU (example)\n");
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"CPU: QEMU Virtual CPU (example)\n");
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     }
 
     if (info_mask & KFETCH_NUM_CPUS) {
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "CPUs: %d / %d\n", num_online_cpus(), num_possible_cpus());
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"CPUs: %d / %d\n", num_online_cpus(), num_possible_cpus());
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     }
 
     if (info_mask & KFETCH_MEM) {
         si_meminfo(&si);
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "Mem: %lu MB / %lu MB\n",
-                             si.freeram >> 10, si.totalram >> 10);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"Mem: %lu MB / %lu MB\n",si.freeram >> 10, si.totalram >> 10);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     }
 
     if (info_mask & KFETCH_NUM_PROCS) {
         for_each_process(task)
             proc_count++;
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "Procs: %d\n", proc_count);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"Procs: %d\n", proc_count);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
     }
 
     if (info_mask & KFETCH_UPTIME) {
-        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,
-                             "Uptime: %lu mins\n", (unsigned long) jiffies_to_msecs(get_jiffies_64()) / 60000);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"Uptime: %lu mins\n", (unsigned long) jiffies_to_msecs(get_jiffies_64()) / 60000);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
+    }
+
+    while(line<=7){
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,linux_logo[line++]);
+        buf_len += scnprintf(kfetch_buf + buf_len, KFETCH_BUF_SIZE - buf_len,"\n");
+    }
+
     }
 
     mutex_unlock(&kfetch_mutex);
@@ -100,11 +106,10 @@ static ssize_t kfetch_read(struct file *file, char __user *user_buf, size_t len,
     if (copy_to_user(user_buf, kfetch_buf, buf_len))
         return -EFAULT;
 
-    *offset = buf_len; // 更新偏移量
+    *offset = buf_len;
     return buf_len;
 }
 
-// 設備文件操作 - write
 static ssize_t kfetch_write(struct file *file, const char __user *user_buf, size_t len, loff_t *offset) {
     int mask_info;
 
@@ -115,13 +120,13 @@ static ssize_t kfetch_write(struct file *file, const char __user *user_buf, size
         return -EFAULT;
 
     mutex_lock(&kfetch_mutex);
-    info_mask = mask_info; // 設定 info_mask
+    info_mask = mask_info;
     mutex_unlock(&kfetch_mutex);
 
     return len;
 }
 
-// 設備文件操作 - open 和 release
+
 static int kfetch_open(struct inode *inode, struct file *file) {
     return 0;
 }
@@ -130,7 +135,6 @@ static int kfetch_release(struct inode *inode, struct file *file) {
     return 0;
 }
 
-// 定義 file_operations
 static struct file_operations kfetch_ops = {
     .owner = THIS_MODULE,
     .read = kfetch_read,
@@ -147,9 +151,9 @@ static int __init kfetch_init(void) {
     }
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
-    cls = class_create(KFETCH_DEV_NAME); // 較新版本只需提供名稱
+    cls = class_create(KFETCH_DEV_NAME);
 #else
-    cls = class_create(THIS_MODULE, KFETCH_DEV_NAME); // 舊版本需要指定模組
+    cls = class_create(THIS_MODULE, KFETCH_DEV_NAME);
 #endif
     if (IS_ERR(cls)) {
         unregister_chrdev(major_number, KFETCH_DEV_NAME);
@@ -178,7 +182,7 @@ static int __init kfetch_init(void) {
 
 
 static void __exit kfetch_exit(void) {
-    kfree(kfetch_buf); // 釋放緩衝區
+    kfree(kfetch_buf);
     device_destroy(cls, MKDEV(major_number, 0));
     class_destroy(cls);
     unregister_chrdev(major_number, KFETCH_DEV_NAME);
@@ -191,6 +195,3 @@ module_exit(kfetch_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("313551122");
 MODULE_DESCRIPTION("system information fetching and a linux logo");
-
-
-
